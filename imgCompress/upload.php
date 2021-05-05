ab76c92f2a6bbbdc79351fa14d9548616640db0a
uzAndijan <uzguitarist98@gmail.com>
2021-05-05 18:20:19 +0500

    SecondCommit
    blabla

--- imgCompress/upload.php
+++ imgCompress/upload.php
@@ -3,9 +3,7 @@ move_uploaded_file($_FILES['file']['tmp_name'],'uploads/'.$_FILES['file']['name'
 $orgfile='uploads/'.$_FILES['file']['name'];
 list($width,$height)=getimagesize($orgfile);
 $newfile=imagecreatefromjpeg($orgfile);
-$newwidth=$width*0.25;
-$newheight=$height*0.25;
-$thumb='uploads/smth'.$_FILES['file']['name'];
+$thumb='uploads/resized'.$_FILES['file']['name'];
 $truecolor=imagecreatetruecolor('300','200');
 imagecopyresampled($truecolor,$newfile,0,0,0,0,'300','200',$width,$height);
 imagejpeg($truecolor,$thumb,100);
